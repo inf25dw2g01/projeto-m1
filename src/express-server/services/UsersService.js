e9f57ddf84e5dd2ca2f703e114302916b668e8cb
romeolp <romeu.lopespinto@gmail.com>
2026-05-10 15:19:44 +0100

    Fix de redundancia no userservices

--- src/express-server/services/UsersService.js
+++ src/express-server/services/UsersService.js
@@ -1,41 +1,18 @@
 /* eslint-disable no-unused-vars */
 const Service = require("./Service");
 const User = require("../models/User");
-const crypto = require("crypto");
 /**
 * Devolve as informações do perfil do atleta autenticado
 *
 * returns User
 * */
 const getAuthenticatedUser = async (req) => {
-  let user = null;
-
-  // OAuth2 (GitHub ou Google)
   if (req.user) {
     user = req.user;
   }
-
-  // (API Key)
-  if (!user) {
-    const apiKey = req.headers['x-api-key'];
-    if (apiKey) {
-      user = await User.findOne({ where: { apiKey: apiKey } });
-    }
-  }
-
-  // PORTA 3: Basic Auth
-  if (!user) {
-    const authHeader = req.headers['authorization'];
-    if (authHeader && authHeader.startsWith('Basic ')) {
-      //
-    }
-  }
-  if (!user) {
-    throw { status: 401, message: "Acesso negado. Autentica-te via OAuth, API Key ou Basic Auth." };
-  }
-
-  return user;
+  throw {status:401, Message: "Acesso negado. Autentica-te via OAuth, API Key ou Basic Auth."};
 };
+
 const usersMeGET = (req) => new Promise(async (resolve, reject) => {
     try {
       const user = await getAuthenticatedUser(req);
@@ -45,7 +22,6 @@ const usersMeGET = (req) => new Promise(async (resolve, reject) => {
       reject(Service.rejectResponse(e.message || 'Erro interno', e.status || 500));
     }
 })
-
 
 module.exports = {
   usersMeGET,
